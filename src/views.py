dd6210a3f20e411c23bed919be5afcc07ba0d812
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-04-10 09:49:05 +0200

    Update views.py
    Co-Authored-By: Stemgon <165911512+stemgon@users.noreply.github.com>
    Co-Authored-By: Carrington Muleya <79579279+carrington-dev@users.noreply.github.com>

--- src/views.py
+++ src/views.py
@@ -1,1 +1,22 @@
 from src import app
+from flask import render_template
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
