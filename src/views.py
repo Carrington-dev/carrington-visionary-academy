c4203189cae49ec1c78c67216cbc422f872cfaf4
Carrington Muleya <79579279+Carrington-dev@users.noreply.github.com>
2025-04-10 18:29:51 +0200

    Added Academic Excellence
    Co-Authored-By: Stemgon <165911512+stemgon@users.noreply.github.com>
    Co-Authored-By: Carrington Muleya <79579279+carrington-dev@users.noreply.github.com>

--- src/views.py
+++ src/views.py
@@ -9,9 +9,9 @@ def inject_globals():
         "company": company,
         "year": 2025,
         "phone": '+27 (0) 67 735 2242',
-        "title": 'Your Gateway to Adventure!',
+        "title": 'Independent Private Schools In South Africa!',
         "email": 'hello@carringtonacademy.co.za',
-        "address": "395 Francis Baard Street</p><p>Pretoria Central, 0001/2</p><p>South Africa",
+        "address": "395 Francis Baard Street</p><p>Pretoria Central, 0001</p><p>South Africa",
         "copyright_notice": f"© {datetime.now().year} { company }. All rights reserved.",
         "copyright": f"""© <span>{datetime.now().year}</span><strong class="px-1 sitename">{ company }.</strong> <span>All Rights Reserved.</span>""",
     }
